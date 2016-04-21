c1cff075d185ef1e7eac8f15e1866a73c7b8ae54
Ronald Hunter <ronald@ronaldhunter.co>
2016-04-21 15:32:34 -0400

    added instructions to be used to build the project, also finished the first part of the projects javascript code

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 jQuery(document).ready(function($) {
-	
+//Store colors
+
+var color = $('.selected').css('background-color');
+
+
+
+	//When clicking on control list items
+	$('.controls li').click(function() {
+		//Deselect sibling elements
+		$(this).siblings().removeClass('selected');
+		//select clicked element
+		$(this).addClass('selected');
+		//Cache current element
+		color = $(this).css('background-color');
+	});
+
+
+	//When add color is pressed
+		//Show color select or hide the color select
+
+	//When color sliders change
+		//update the new color 
+
+	//when add color is pressed
+		//append the color to the controls 
+		//Select the new color
+
+	// On mouse events on the canvas
+		// Draw lines
 });
