776ca868080eb592e2bb5e5c153d739e1e67a4aa
Ronald Hunter <ronald@ronaldhunter.co>
2016-04-22 11:44:30 -0400

    Added .on to the controls so the newly appened color will be selectable.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var color = $('.selected').css('background-color');
 
 
 	//When clicking on control list items
-	$('.controls li').click(function() {
+	$('.controls').on('click',"li",function() {
 		//Deselect sibling elements
 		$(this).siblings().removeClass('selected');
 		//select clicked element
@@ -17,14 +17,39 @@ var color = $('.selected').css('background-color');
 
 
 	//When add color is pressed
+	$('#revealColorSelect').click(function() {
+		changeColor();
 		//Show color select or hide the color select
+		$('#colorSelect').toggle()
+	});
+
+
+
+
+	//update the new color 		
+	function changeColor(argument) {
+		var r = $('#red').val();
+		var g = $('#green').val();
+		var b = $('#blue').val();
+
+		$('#newColor').css('background-color', 'rgb('+ r +','+ g +','+ b +')');
+	}
 
 	//When color sliders change
-		//update the new color 
+	$('input[type="range"]').change(changeColor);
+
 
 	//when add color is pressed
+	$('#addNewColor').click(function() {
 		//append the color to the controls 
+		var $newColor = $('<li></li>');
+		$newColor.css('background-color', $('#newColor').css('background-color'));
+		$('.controls ul').append($newColor)
 		//Select the new color
+		$newColor.click();
+	});
+		
+		
 
 	// On mouse events on the canvas
 		// Draw lines
