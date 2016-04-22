5cd4c1004208a7d4a9b6ecc6cf8781406c595283
Ronald Hunter <ronald@ronaldhunter.co>
2016-04-22 12:34:45 -0400

    added the ability to draw on the canvas and switch between the colors to use to draw.

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ jQuery(document).ready(function($) {
 //Store colors
 
 var color = $('.selected').css('background-color');
-
+var context = $('canvas')[0].getContext('2d');
+var $canvas = $('canvas');
+var lastEvent;
+var mouseDown = false;
 
 
 	//When clicking on control list items
@@ -52,5 +55,29 @@ var color = $('.selected').css('background-color');
 		
 
 	// On mouse events on the canvas
+	$canvas.mousedown(function(e) {
+
+		lastEvent = e;
+		mouseDown = true;
+		
+	}).mousemove(function(e) {
 		// Draw lines
+		if (mouseDown) {
+			context.beginPath();
+			context.moveTo(lastEvent.offsetX,lastEvent.offsetY);
+			context.lineTo(e.offsetX, e.offsetY);
+			context.strokeStyle = color;
+			context.stroke();
+			lastEvent = e;
+
+		}
+
+	}).mouseup(function() {
+		mouseDown = false;
+	}).mouseleave(function() {
+		$canvas.mouseup();
+	});
+
+
+
 });
